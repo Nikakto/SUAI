ed4034473e4d14c940f650164d65e8a0f75ad785
Nikakto <black-z@bk.ru>
2017-11-26 22:03:56 +0300

    Lena

--- SUAI/static/js/main.js
+++ SUAI/static/js/main.js
@@ -2,15 +2,14 @@ function convert() {
     
     obj_from = document.getElementById("converter-unit-from-value");
     value = Number(obj_from.value);
-    
-    meters_from_obj = document.getElementById("converter-unit-from");
-    meters_from = Number(meters_from_obj.value);
-    
-    meters_to_obj = document.getElementById("converter-unit-to");
-    meters_to = Number(meters_to_obj.value);
-    
-    obj_result = document.getElementById("converter-unit-to-value");
-    obj_result.value = value*meters_from / meters_to;
+
+    if (value) {
+        item = document.getElementById("calc_result")
+        item.innerHTML = "Вам уже " +value*7+ " по собачьим меркам!";
+    }
+    else {
+        item.innerHTML = "Пожалйста, введите число!";
+    }
     
 }
 
@@ -25,26 +24,4 @@ function getName(str){
     var filename = str.slice(i);
     var uploaded = document.getElementById("fileformlabel");
     uploaded.innerHTML = filename;
-}
-
-
-function news_addform_close() {
-
-    button_add = document.getElementById("button_create");
-    button_add.style.removeProperty('display');
-
-    form_add = document.getElementsByClassName("news-add")[0];
-    form_add.style.display = 'None';
-
-}
-
-
-function news_addform_show() {
-
-    button_add = document.getElementById("button_create");
-    button_add.style.display = 'None';
-
-    form_add = document.getElementsByClassName("news-add")[0];
-    form_add.style.removeProperty('display');
-
 }
